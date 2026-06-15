3155221b138e3df51d2c46e3a3d6f067f816310c
stankovvv <mykola.stankov07@gmail.com>
2026-06-15 16:58:08 +0300

    part1 done

--- Program.cs
+++ Program.cs
@@ -24,3 +24,27 @@ int jeong2 = 92;
 int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
+
+
+// declare int for each student to sum
+// value assigment isnt mecessary but it boost efficiency and readability of code
+// check the code
+// int sophiaSum = 0;
+// int nicolasSum = 0;
+// int zahirahSum = 0;
+// int jeongSum = 0;
+
+// grade calculation
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+// show output (for insuring the code is working as intended)
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas:" + nicolasSum);
+Console.WriteLine("ZahirahSum: " + zahirahSum);
+Console.WriteLine("Jeong:" + jeongSum);
+    
+
